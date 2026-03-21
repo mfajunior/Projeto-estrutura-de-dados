45a1ac15881a677ba966aa441317c70049ba1139
Milton Faria <mfajunior1@gmail.com>
2026-03-21 19:17:32 -0300

    Projeto finalizado, todas funcoes implementadas, necessario teste e gravacao de video

--- estacionamento.c
+++ estacionamento.c
@@ -1,187 +1,261 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #include <windows.h>
+
+int contador_id = 1; // Variavel global para automatizar contador
 
 typedef struct estacionamento{
 	int id;
 	int vaga;                
 	char cor[15];    
 	char placa[9];   
-	char nome[20];
-	bool tipo;               
+	char nome[20];              
 } estacionamento;
-
 
 typedef struct no{
     estacionamento carro;
     struct no *next;
 } no;
 
-
 no* cria() {
     return NULL;
 }
 
+void desenhaLogoUSF() {
+    printf("\n");
+    printf("        ########                                            \n");
+    printf("       #######    ######    ############    %%##############\n");
+    printf("      ######    ########  ################  ################\n");
+    printf("      ######    ########  ######    ######  ######        \n");
+    printf("     #######    ######    ######    ######  ######       \n");
+    printf("    ########    ######    ########          ######        \n");
+    printf("    #######     ######    ##########        ##############  \n");
+    printf("    ######      ######      ##########mm    ##############  \n");
+    printf("    ######     #######        ##########    ######          \n");
+    printf("    ######    ########          ########    ######          \n");
+    printf("   #######    ########  ######    ######    ######         \n");
+    printf("   #######    ######## #######    ######    ######          \n");
+    printf("   #################  ##################    ######          \n");
+    printf("    ##############      ############        ######            \n");
+    printf("      ##########          ######                            \n");
+    printf("\n");
+    printf("           SISTEMA DE ESTACIONAMENTO - USF\n");
+    printf("    ------------------------------------------------");
+}
+
 no* insere(no* database) {
     no* novo = (no*)malloc(sizeof(no));
+    if (novo == NULL) return database;
+
     system("cls");
+    printf("\n--- Cadastro da Vaga ---\n");
     
-	printf("\n--- Cadastro da Vaga Ocupada ---\n");
-    printf("ID: ");
-    scanf("%i", &novo->carro.id);
+    novo->carro.id = contador_id;
+    contador_id = contador_id+1;
     
     printf("Qual a cor do veiculo?: ");
-    scanf("%s",&novo->carro.cor);
+    scanf("%s", novo->carro.cor);
     
-    printf("Qual a placa do veiculo? Digite no formato ""AAA-AAAA"": ");
-    scanf("%s",&novo->carro.placa);
+    printf("Qual a placa do veiculo? (AAA-AAAA): ");
+    scanf("%s", novo->carro.placa);
     
     printf("Qual o numero da vaga?: ");
     scanf("%i", &novo->carro.vaga);
 
-    printf("Nome do aluno/funcionario que esta ocupando a vaga: ");
-    scanf("%s", &novo->carro.nome);
+    printf("Nome do proprietario: ");
+    scanf("%s", novo->carro.nome);
 
-    printf("Digite 1 - Moto / 2 - Carro: ");
-    scanf("%d", &novo->carro.tipo);
-    
     novo->next = database;
-    system ("cls");
+    system("cls");
 	return novo;
 }
 
-
 no* altera(no* database, int id_procurado) {
     no* aux = database;
-    
     for (aux = database; aux != NULL; aux = aux->next) {
         if (aux->carro.id == id_procurado){
-        	printf("\n--- Alterar informacoes da vaga ocupada ---\n");
-		    printf("ID a ser alterado: %i\n", aux->carro.id );
-		    
-		    printf("Qual a cor do veiculo?: ");
-		    scanf("%s",&aux->carro.cor);
-		    
-		    printf("Qual a placa do veiculo? Digite no formato ""AAA-AAAA"": ");
-		    scanf("%s",&aux->carro.placa);
-		    
-		    printf("Qual o numero da vaga?: ");
+        	printf("\n--- Alterar informacoes (ID: %i) ---\n", aux->carro.id);
+		    printf("Nova cor: ");
+		    scanf("%s", aux->carro.cor);
+		    printf("Nova placa: ");
+		    scanf("%s", aux->carro.placa);
+		    printf("Nova vaga: ");
 		    scanf("%i", &aux->carro.vaga);
-		
-		    printf("Nome do aluno/funcionario que esta ocupando a vaga: ");
-		    scanf("%s", &aux->carro.nome);
-		
-		    printf("Digite 1 - Moto / 2 - Carro: ");
-		    scanf("%i", &aux->carro.tipo);
-		    
-		    system ("cls");
-			return aux;
-        	
-		};
+		    printf("Novo nome: ");
+		    scanf("%s", aux->carro.nome);
+			return database;
+		}
     }
-    printf("ID %i nao encontrado! \n", id_procurado);
+    printf("ID %i nao encontrado!\n", id_procurado);
+    system("cls");
+    return database;
 }
 
+void reordenaID(no* database) {
+    if (database == NULL) { // Se a databaes estiver vazia, o valor do contador é setado como 1 referente ao primeiro ID
+        contador_id = 1;
+        return;
+    }
+
+    int total = 0;
+    no* aux = database;
+    while (aux != NULL) { // Conta quantos IDs estão existentes no dabase
+        total = total +1;
+        aux = aux->next;
+    }
+
+    contador_id = total + 1; // Seta a variavel global contador_id para o próximo valor a ser inserido
+
+    aux = database; //aux recebe novamente a database para recomeçar o loop
+    while (aux != NULL) { //como a estrutura de dados de uma lista encadeada é uma "pilha" o loop começa do maior valor para o menor
+        aux->carro.id = total;
+		total = total -1; 
+        aux = aux->next;
+    }
+}
 
 no* retira(no *database, int id_remover){
 	no* ant = NULL;
 	no* aux = database;
+	
 	while (aux != NULL && aux->carro.id != id_remover){
-	ant = aux;
-	aux = aux->next;
+	    ant = aux;
+	    aux = aux->next;
 	}
-	if (ant == NULL) database= aux->next;
+    
+    if (aux == NULL) {
+    	system("cls");
+		printf("Veiculo nao encontrado! \n");
+		Sleep(800);	
+		return database;	
+	}
+		
+
+	if (ant == NULL) database = aux->next;
     else ant->next = aux->next;
+    
     free(aux);
-    system ("cls");
+
+    if (id_remover == contador_id - 1) {
+        contador_id = contador_id - 1;
+    }
+    reordenaID(database);
+
+    system("cls");
     return database;
 }
 
 void imprime(no* database) {
     no* aux;
-    
     if (database == NULL) {
         printf("\nEstacionamento vazio!\n");
         return;
     }
-
-    printf("\n--- Veiculos no estacionamento ---\n");
-    // O sinal de menos (-) alinha a esquerda. O numero define o tamanho da coluna.
-    printf("%-4s | %-10s | %-8s | %-12s | %-4s | %-5s\n", "ID", "Placa", "Cor", "Nome", "Vaga", "Tipo");
-    printf("------------------------------------------------------------\n");
-
+    printf("\n        --- Veiculos no estacionamento ---\n");
+    printf("%-4s | %-10s | %-8s | %-12s | %-4s\n", "ID", "Placa", "Cor", "Nome", "Vaga");
+    printf("---------------------------------------------------\n");
     for (aux = database; aux != NULL; aux = aux->next) {
-        printf("%-4i | %-10s | %-8s | %-12s | %-4i | %-5i\n",
-               aux->carro.id, 
-               aux->carro.placa, 
-               aux->carro.cor, 
-               aux->carro.nome,
-			   aux->carro.vaga, 
-               aux->carro.id); // Usei id_tipo aqui como exemplo do 1 ou 2
+        printf("%-4i | %-10s | %-8s | %-12s | %-4i\n",
+               aux->carro.id, aux->carro.placa, aux->carro.cor, 
+               aux->carro.nome, aux->carro.vaga);
     }
-    printf("------------------------------------------------------------\n");
+    printf("---------------------------------------------------\n");
 }
 
-void* libera(no* database) {
+no* libera(no* database) {
     no* aux = database;
     while (aux != NULL) {
         no* temp = aux->next;
         free(aux);
         aux = temp;
     }
+
+    contador_id = 1; 
     return NULL;
+}
+
+void retornamenu(){
+	printf("Retornando ao menu principal.");
+	Sleep(400);
+	printf(".");
+	Sleep(400);
+	printf(".");
+	Sleep(400);
+	printf(".");
+	Sleep(400);
+	printf(".");
+	Sleep(400);
+	printf(".");
+	Sleep(400);
+	printf(".");
+	Sleep(400);
+	printf(".");
+	system("cls");
 }
 
 int main (){
 	no* base = cria();
-	int opcao=0;
+	int opcao = 0;
   
     while (opcao != 6) {
-        printf("\n1. Mostrar todos os dados \n2. Incluir dado \n3. Apagar dado \n4. Alterar dado \n5. Apagar tudo \n6. Sair\nDigite a opcao desejada: ");
+    	desenhaLogoUSF();
+    	system("color 1F");
+        printf("\n1. Mostrar veiculos no estacionamento \n2. Incluir veiculo \n3. Apagar um veiculo \n4. Alterar dados de um veiculo \n5. Apagar tudo \n6. Sair\nOpcao: ");
         scanf("%d", &opcao);
-        int retorno=0;
-		int operador=0;
+        int retorno = 0;
+		int operador = 0;
 		
         switch (opcao) {
-            case 1: // Ok
+            case 1:
                 system("cls");	
                 imprime(base);
-                printf("Digite 1 para voltar ao menu principal:");
-                while(retorno!=1) scanf("%i", &retorno); 
+                printf("\nDigite 1 para voltar: ");
+                while(retorno != 1) scanf("%i", &retorno); 
                 system("cls");
                 break;
-            case 2: // Ok
+            case 2:
                 base = insere(base);
                 break;
-            case 3: // OK
-                system ("cls");
+            case 3:
+                system("cls");
+                if (base == NULL) {
+			        printf("\nEstacionamento vazio!\n");
+			        retornamenu();
+			        break;
+  					}
                 imprime(base);
                 printf("Digite o ID para apagar: ");
                 scanf("%d", &operador);
                 base = retira(base, operador);
                 break;
-            case 4: // OK
-                system ("cls");
+            case 4:
+                system("cls");
+                if (base == NULL) {
+			        printf("\nEstacionamento vazio!\n");
+			        retornamenu();
+			        break;
+	  				}
                 imprime(base);
                 printf("Digite o ID para alterar: ");
                 scanf("%d", &operador);
                 base = altera(base, operador);
                 break;
-            case 5: // OK
-                base = libera(base);
-                printf("Todos os dados foram apagados!\n");
-                sleep(1);
-                system ("cls");
+            case 5:
+            	system("cls");
+            	system("color 4F");
+            	printf("Voce tem certeza que quer apagar TODOS os registros? 1 - SIM / 2 - NAO\n");
+            	scanf("%i",&operador);
+            	system("cls");
+            	if (operador == 1){                
+					base = libera(base);
+	                printf("Todos os dados foram apagados !");
+	                Sleep(1000);
+				}
+				else retornamenu();			
+
                 break;
-            case 6:
-                break;
-            default:
-                printf("Opcao invalida!\n");
         }
     }
-
-    printf("Fim do programa.\n");
     return 0;
 }
